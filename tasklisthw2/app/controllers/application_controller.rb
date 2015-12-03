59662794060b35e7d6883650d85b4ada12739713
ydu5 <LYD@Lucys-MacBook-Pro.local>
2015-12-03 03:00:09 -0500

    layoutsss

--- tasklisthw2/app/controllers/application_controller.rb
+++ tasklisthw2/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_all_task_lists
+  layout 'todo_list'
+
+def get_all_task_lists
+	@all_task_lists = Tasklist.all	
+end
+
 end
